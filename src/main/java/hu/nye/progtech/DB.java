215552d8b07bcb023b39356be0ca3e75818531c2
Nemeth Janos <nemethjanos9107@gmail.com>
2024-01-08 14:47:44 +0100

    messagere

--- src/main/java/hu/nye/progtech/DB.java
+++ src/main/java/hu/nye/progtech/DB.java
@@ -6,9 +6,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DB {
-    private static final String URL = "jdbc:mysql://localhost:3306/highscore"; // Az adatbázis nevével helyettesítsd
-    private static final String USER = "root"; // A MySQL felhasználóneveddel helyettesítsd
-    private static final String PASSWORD = ""; // A MySQL jelszavaddal helyettesítsd
+    private static final String URL = "jdbc:mysql://localhost:3306/highscore"; //
+    private static final String USER = "root"; //
+    private static final String PASSWORD = ""; // A
 
     public static void main(String[] args) {
         readDataFromUsersTable();
